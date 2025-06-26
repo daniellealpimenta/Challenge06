16185b57c542a8ef712c0a9f711b680f2d7fb498
Marcos Albuquerque <marcosg.as2232@gmail.com>
2025-06-26 18:01:39 -0300

    Delete Challenge 06 directory

--- Challenge 06/Challenge06/data/SwiftData.swift
+++
@@ -1,7 +0,0 @@
-//
-//  SwiftData.swift
-//  Challenge06
-//
-//  Created by Daniel Leal PImenta on 26/06/25.
-//
-
--- Challenge 06/Challenge06/view/screens/Home.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Home.swift
-//  Challenge06
-//
-//  Created by Daniel Leal PImenta on 26/06/25.
-//
-
