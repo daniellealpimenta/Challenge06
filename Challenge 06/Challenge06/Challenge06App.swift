87b87412089a26e3b3a4434ac7c5174e735e8e23
marcosalbuquerque <marcosg.as2232@gmail.com>
2025-06-26 17:55:33 -0300

    criação do componente conteinerRow

---
+++ Challenge 06/Challenge06/Challenge06App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge06App.swift
+//  Challenge06
+//
+//  Created by Daniel Leal PImenta on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 06/Challenge06/data/SwiftData.swift
@@ -0,0 +1,7 @@
+//
+//  SwiftData.swift
+//  Challenge06
+//
+//  Created by Daniel Leal PImenta on 26/06/25.
+//
+
