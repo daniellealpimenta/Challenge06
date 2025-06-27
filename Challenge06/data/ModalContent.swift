7628c714e6f111891e82e5dbe7451be0762cbdb8
Kenay <kenay@MacKenay.local>
2025-06-27 14:52:52 -0300

    adding files of modal

---
+++ Challenge06/data/ModalContent.swift
@@ -0,0 +1,29 @@
+//
+//  ModalContent.swift
+//  Challenge06
+//
+//  Created by Kenay on 26/06/25.
+//
+
+import Foundation
+
+//Essa é a struct que vai estruturar todo o conteudo da tela. aqui vamos por tudo que vai ter de titulo, subtitulo, fotos e essas coisas ai
+struct ConteudoModal {
+    let title: String
+    let description: String
+    let icon: String
+}
+
+//Como ja esta estruturado em cima o que vai ter na pagina agora é so popular o que realmente de fato vai ter na tela
+let conteudosModal: [ConteudoModal] = [
+    ConteudoModal(
+        title: "Prevenção da Dengue",
+        description: "Evite água parada, tampe tonéis e limpe calhas.",
+        icon: "drop.triangle"
+    ),
+    ConteudoModal(
+        title: "Vacinação Antirrábica",
+        description: "Leve seu pet ao posto de vacinação mais próximo.",
+        icon: "cross.case.fill"
+    )
+]
